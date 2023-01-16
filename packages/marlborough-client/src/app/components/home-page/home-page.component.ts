df924c49703f27bb849732a7ed3f8358ee539f66
Greg Ede <gregory.ede@gmail.com>
2023-01-17 12:34:49 +1300

    Preload large images on the home page

--- packages/marlborough-client/src/app/components/home-page/home-page.component.ts
+++ packages/marlborough-client/src/app/components/home-page/home-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { timer, map } from 'rxjs';
+import { timer, map, Observable } from 'rxjs';
 import { LoadingService } from 'src/app/services/loading.service';
 import { FlightService } from 'src/app/services/flight.service';
+import { Airport } from '@marlborough/model';
 
 @Component({
   selector: 'app-home-page',
@@ -13,13 +14,14 @@ import { FlightService } from 'src/app/services/flight.service';
 })
 export class HomePageComponent implements OnInit {
 
+  origins?: Observable<Airport[]>;
+
   constructor(private _flightService: FlightService) { }
 
   ngOnInit(): void {
     // when we start grab the images for the destinations page early to let appear instant
-    this._flightService.getOrigins().subscribe(o => {
-      const x = o;
-    });
+    // no need for the loading service as this is intended to be in the background
+    this.origins = this._flightService.getOrigins();
   }
 
   /*
