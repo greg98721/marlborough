3d6a9d1bad8db618728884cc1a05c5f6052e1dc0
Greg Ede <gregory.ede@gmail.com>
2023-01-21 13:13:30 +1300

    Made timetable page async

--- packages/marlborough-client/src/app/components/timetable-page/timetable-page.component.ts
+++ packages/marlborough-client/src/app/components/timetable-page/timetable-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { addMinutes, format } from 'date-fns/fp';
+import { Observable, map } from 'rxjs';
 import { Airport, cityName, isAirport, TimetableFlight } from '@marlborough/model';
 import { WeekDisplayComponent } from '../week-display/week-display.component';
 
@@ -13,31 +14,41 @@ import { WeekDisplayComponent } from '../week-display/week-display.component';
   styleUrls: ['./timetable-page.component.scss']
 })
 export class TimetablePageComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
-  origin = '';
-  timetables: { destination: string; destCode: string; timetables: TimetableFlight[] }[] = [];
+  origin?: Observable<string>;
+  timetables?: Observable<{ destination: string; destCode: string; timetables: TimetableFlight[] }[]>;
 
   ngOnInit(): void {
-    const airport = this.route.snapshot.paramMap.get('airport');
-    if (airport && isAirport(airport)) {
-      this.origin = cityName(airport);
-    }
-    const timetables = this.route.snapshot.data['airport'] as TimetableFlight[];
+    this.origin = this.route.paramMap.pipe(
+      map(o => {
+        const airport = o.get('airport');
+        if (airport && isAirport(airport)) {
+          return cityName(airport);
+        } else {
+          return '';
+        }
+      })
+    );
+    this.timetables = this.route.data.pipe(
+      map(t => {
+        const timetables = t['airport'] as TimetableFlight[];
 
-    // get the unique destinations
-    const a = timetables.map(t => t.route.destination);
-    const unique =[...new Set(a)];
+        // get the unique destinations
+        const a = timetables.map(t => t.route.destination);
+        const unique = [...new Set(a)];
 
-    this.timetables = unique.map(u => {
-      const name = cityName(u);
-      const s = timetables.filter(t => t.route.destination === u).sort((a, b) => a.departs >= b.departs ? 1 : -1 );
-      return { destination: name, destCode: u, timetables: s };
-    }).sort((a, b) => a.destination.localeCompare(b.destination));
+        return unique.map(u => {
+          const name = cityName(u);
+          const s = timetables.filter(t => t.route.destination === u).sort((a, b) => a.departs >= b.departs ? 1 : -1);
+          return { destination: name, destCode: u, timetables: s };
+        }).sort((a, b) => a.destination.localeCompare(b.destination));
+      })
+    )
   }
 
   formatTime(minutes: number): string {
-    // we need a date, event though it will disappear when we format it again
+    // we need a date, even though it will disappear when we format it again
     return format('p', addMinutes(minutes, new Date(2023, 1, 1, 0, 0, 0)));
   }
 
