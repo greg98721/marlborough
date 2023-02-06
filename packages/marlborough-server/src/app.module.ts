84f4510c0376640a83c92fda7f3288a9d435a342
Greg Ede <gregory.ede@gmail.com>
2023-02-06 17:42:48 +1300

    Static serving of client added

--- packages/marlborough-server/src/app.module.ts
+++ packages/marlborough-server/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AirRoutesController } from './air-routes/airRoutes.controller';
 import { ScheduleService } from './schedule/schedule.service';
 import { FlightsController } from './flights/flights.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(
+        __dirname,
+        '..',
+        '..',
+        'marlborough-client',
+        'dist',
+        'marlborough-client',
+      ),
+    }),
+  ],
   controllers: [AirRoutesController, FlightsController],
-  providers: [ ScheduleService],
+  providers: [ScheduleService],
 })
 export class AppModule {}
